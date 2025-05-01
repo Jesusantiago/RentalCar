6480a5e9a047793f58c0da61e542a2bf8abbabc9
Jesus Santiago <jesus-contreras1@eep-igroup.com>
2025-05-01 18:56:39 +0200

    ADD test for getUserById

--- LOGIN/src/test/java/com/proyectofinal/login/loginservice/controller/UserControllerTest.java
+++ LOGIN/src/test/java/com/proyectofinal/login/loginservice/controller/UserControllerTest.java
@@ -9,10 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.time.LocalDate;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -38,8 +41,39 @@ public class UserControllerTest {
         user.setDateOfBirth(LocalDate.now());
 
         mockMvc.perform(post("/api/users/register")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(user)))
+            .andExpect(status().isCreated());
+    }
+
+    @Test
+    void getUserById_shouldReturn200() throws Exception {
+        User user = new User();
+        user.setName("test");
+        user.setLastName("example");
+        user.setEmail("test@proyectofinal.com");
+        user.setPassword("password123");
+        user.setUserName("testValid");
+        user.setLicenseType("B");
+        user.setDateOfBirth(LocalDate.now());
+
+        mockMvc.perform(post("/api/users/register")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isCreated());
+                .content(objectMapper.writeValueAsString(user)));
+
+        MvcResult resultJwt = mockMvc.perform(post("/api/auth/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"email\": \"test@proyectofinal.com\", \"password\": \"password123\"}"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.token").isNotEmpty())
+            .andReturn();
+
+        String responseContent = resultJwt.getResponse().getContentAsString();
+
+        String jwtToken = objectMapper.readTree(responseContent).get("token").asText();
+
+        mockMvc.perform(get("/api/users/1")
+            .header("Authorization", "Bearer " + jwtToken))
+            .andExpect(status().isOk());
     }
 }
