8d19d0209391ceaacfdad1f8f46882de230105e3
Jesus Santiago <jesus-contreras1@eep-igroup.com>
2025-05-04 00:57:58 +0200

    ADD new method for search

--- BACK/src/main/java/com/proyectofinal/car/repository/BranchRepository.java
+++ BACK/src/main/java/com/proyectofinal/car/repository/BranchRepository.java
@@ -1,9 +1,19 @@
 package com.proyectofinal.car.repository;
 
 import com.proyectofinal.car.model.Branch;
+import com.proyectofinal.car.model.Rental;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BranchRepository extends JpaRepository<Branch, Long> {
+    Optional<Branch> findByNameContainingIgnoreCase(String name);
+    Optional<Branch> findByAddressContainingIgnoreCase(String address);
+    Optional<Branch> findByCityContainingIgnoreCase(String city);
+    Optional<Branch> findByPhone(int phone);
 }
+
