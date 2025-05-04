c9f4f311bbc280c7e15d8b037e6a0b7f779e5d1c
Jesus Santiago <jesus-contreras1@eep-igroup.com>
2025-05-04 20:28:59 +0200

    FIX test for funcional with order and direcional

--- BACK/src/test/java/com/proyectofinal/car/service/CarServiceTest.java
+++ BACK/src/test/java/com/proyectofinal/car/service/CarServiceTest.java
@@ -7,6 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
@@ -18,20 +23,20 @@ public class CarServiceTest {
 
     @Test
     void getAvailableCars_shouldNotBeEmpty() {
-        Page<CarPreviewDTO> result = carService.getAvailableCars(0,10);
+        Page<CarPreviewDTO> result = carService.getAvailableCars(0,10, null, null);
         assertThat(result).isNotNull();
         assertThat(result).isNotEmpty();
     }
 
     @Test
     void getAvailableCars_shouldReturnListOfCars() {
-        Page<CarPreviewDTO> result = carService.getAvailableCars(0,10);
+        Page<CarPreviewDTO> result = carService.getAvailableCars(0,10, null, null);
         assertThat(result.getContent().size()).isEqualTo(8);
     }
 
     @Test
-    void getAvailableCars_CheckDataWithCarDTO(){
-        Page<CarPreviewDTO> result = carService.getAvailableCars(0,10);
+    void getAvailableCars_checkDataWithCarDTO(){
+        Page<CarPreviewDTO> result = carService.getAvailableCars(0,10, null, null);
         assertThat(result.getContent().get(0).getModel()).isEqualTo("Supra");
         assertThat(result.getContent().get(0).getBrand()).isEqualTo("Toyota");
         assertThat(result.getContent().get(0).getBranchCity()).isEqualTo("Merida");
@@ -39,9 +44,29 @@ public class CarServiceTest {
     }
 
     @Test
-    void getAvailableCars__ShouldReturnEmptyPage_WhenPageNumberIsTooHigh(){
-        Page<CarPreviewDTO> result = carService.getAvailableCars(1,10);
+    void getAvailableCars__shouldReturnEmptyPage_WhenPageNumberIsTooHigh(){
+        Page<CarPreviewDTO> result = carService.getAvailableCars(1,10, null, null );
         assertThat(result).isEmpty();
+    }
+
+    @Test
+    void getAvailableCars_checkDataWithSort(){
+        Page<CarPreviewDTO> result = carService.getAvailableCars(0, 10, "model", "asc" );
+
+        // Extraemos los modelos
+        List<String> actualModels = result.getContent()
+                .stream()
+                .map(CarPreviewDTO::getModel)
+                .collect(Collectors.toList());
+
+        // Hacemos una copia y la ordenamos manualmente para comparar
+        List<String> expectedSortedModels = new ArrayList<>(actualModels);
+        expectedSortedModels.sort(Comparator.naturalOrder());
+
+        // Comprobamos que el orden es el esperado
+        assertThat(actualModels).isEqualTo(expectedSortedModels);
+
+
     }
 
 
