8e7d77aa2c2b5b942922c7f7b3ac8a04bb878932
Jesus Santiago <jesus-contreras1@eep-igroup.com>
2025-05-04 22:25:10 +0200

    ADD test for CarService for one car per Id

--- BACK/src/test/java/com/proyectofinal/car/service/CarServiceTest.java
+++ BACK/src/test/java/com/proyectofinal/car/service/CarServiceTest.java
@@ -53,20 +53,15 @@ public class CarServiceTest {
     @Test
     void getAvailableCars_checkDataWithSort(){
         Page<CarPreviewDTO> result = carService.getAvailableCars(0, 10, "model", "asc" );
-
-        // Extraemos los modelos
         List<String> actualModels = result.getContent()
                 .stream()
                 .map(CarPreviewDTO::getModel)
                 .collect(Collectors.toList());
 
-        // Hacemos una copia y la ordenamos manualmente para comparar
         List<String> expectedSortedModels = new ArrayList<>(actualModels);
         expectedSortedModels.sort(Comparator.naturalOrder());
 
-        // Comprobamos que el orden es el esperado
         assertThat(actualModels).isEqualTo(expectedSortedModels);
-    }
     }
 
     @Test
@@ -82,10 +77,6 @@ public class CarServiceTest {
     }
 
     @Test
-    void getCarById_noShouldNullOrEmpty() {
-        CarDetailsDTO car = carService.getCarById(1L);
-        assertThat(car).isNotNull();
-    @Test
     void getCarById_shouldReturnCar() {
         CarDetailsDTO car = carService.getCarById(1L);
         assertThat(car).isNotNull();
@@ -97,6 +88,8 @@ public class CarServiceTest {
         assertThat(car.getBranch().getAddress()).isEqualTo("Av. Principal 123");
         assertThat(car.getBranch().getPhone()).isEqualTo("123456789");
     }
+
+
 
 
 }
