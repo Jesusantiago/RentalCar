47fafb2553b60fe863a10ca100d0073380a4040e
Jesus Santiago <jesus-contreras1@eep-igroup.com>
2025-05-12 23:49:53 +0200

    ADD data class for login

--- FRONT-END/app/src/main/java/com/proyectofinal/carrentaladminapp/data/model/LoginRequest.kt
+++ FRONT-END/app/src/main/java/com/proyectofinal/carrentaladminapp/data/model/LoginRequest.kt
@@ -1,3 +1,13 @@
 package com.proyectofinal.carrentaladminapp.data.model
 
-data class LoginRequest()
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+
+data class LoginResponse(
+    val token: String,
+    val idUser: Long,
+    val userName: String,
+    val licenseType: String
+)
